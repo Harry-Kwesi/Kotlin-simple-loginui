bcc123b47a6b05630c923f1c9f49b1de488dd40f
Harry-Kwesi <harrydegraft@hotmail.com>
2024-01-27 22:14:57 +0530

    Simple login form with Kotlin done

---
+++ app/src/main/java/com/example/userloginkotlin/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.userloginkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+import android.util.Log
+
+
+
+class MainActivity : AppCompatActivity() {
+
+     private lateinit var usernameInput : EditText
+     private lateinit var passwordInput : EditText
+     private lateinit var loginBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn  = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test", "Username: $username and Password: $password")
+        }
+    }
+}
